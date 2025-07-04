0eb65453833df423d6692fc25c7ce1c9879b6daf
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-07-04 14:51:48 -0300

    entity criada

---
+++ account/src/main/java/com/sicarus/model/Account.java
@@ -0,0 +1,55 @@
+package com.sicarus.model;
+
+import com.sicarus.enums.AccountType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+//id userId type balance
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+    private AccountType type;
+    private Double balance;
+
+    public Account(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}

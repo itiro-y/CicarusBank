a39c3e9c5ef9064fca6707946a296520a2383853
itiro-y <itiro_yoshii@hotmail.com>
2025-07-06 16:19:36 -0300

    Implemented Controller  - getTransactionById  - postTransaction
    Added TransactionRequestDTO.java

--- transaction/src/main/java/com/sicarus/controller/TransactionController.java
+++ transaction/src/main/java/com/sicarus/controller/TransactionController.java
@@ -1,13 +1,18 @@
 package com.sicarus.controller;
 
+import com.sicarus.dto.TransactionRequestDTO;
 import com.sicarus.model.Transaction;
+import com.sicarus.model.TransactionType;
 import com.sicarus.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("transaction")
@@ -20,5 +25,30 @@ public class TransactionController {
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Transaction> getTransactionById(@PathVariable Long id){
+        return transactionRepository.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postTransaction(@RequestBody TransactionRequestDTO request){
+        if(request.getAmount() == null || request.getAmount() <= 0){
+            return ResponseEntity.badRequest().body("Amount given as parameter is zero/negative/null.");
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setAccountId(request.getAccountId());
+        transaction.setTransactionType(request.getTransactionType());
+        transaction.setAmount(request.getAmount());
+        transaction.setTimestamp(new Date());
+
+        transactionRepository.save(transaction);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Transação criada com sucesso!");
+    }
+
+    // To-Do -> Cacelamento Logico
+
+    // To-Do -> Estorno
 
 }
---
+++ transaction/src/main/java/com/sicarus/dto/TransactionRequestDTO.java
@@ -0,0 +1,33 @@
+package com.sicarus.dto;
+
+import com.sicarus.model.TransactionType;
+
+public class TransactionRequestDTO {
+    private Long accountId;
+    private TransactionType transactionType;
+    private Double amount;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+}

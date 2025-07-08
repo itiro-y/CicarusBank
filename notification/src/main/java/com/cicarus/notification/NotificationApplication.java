208d415025730a47b155a2ecc9592ad7a9492347
Ferrari <vitorferrarijoao5@gmail.com>
2025-07-08 11:49:28 -0300

    notification: explode resolve

--- notification/src/main/java/com/cicarus/notification/NotificationApplication.java
+++ notification/src/main/java/com/cicarus/notification/NotificationApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class StartupNotification {
+public class NotificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StartupNotification.class, args);
+		SpringApplication.run(NotificationApplication.class, args);
 	}
 
 }

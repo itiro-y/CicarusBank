7df1c8686e76072455e49f3bc90390b1a64013d5
Ferrari <vitorferrarijoao5@gmail.com>
2025-07-08 11:38:56 -0300

    notification: openfeigh

--- notification/src/main/java/com/cicarus/notification/StartupNotification.java
+++ notification/src/main/java/com/cicarus/notification/StartupNotification.java
@@ -2,12 +2,14 @@ package com.cicarus.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-public class NotificationApplication {
+public class StartupNotification {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationApplication.class, args);
+		SpringApplication.run(StartupNotification.class, args);
 	}
 
 }

5d105cb7ebbbd7ed6edf14ce517be49b82e17db3
Ferrari <vitorferrarijoao5@gmail.com>
2025-07-09 12:06:40 -0300

    Notification: fixed

--- notification/src/main/java/com/cicarus/notification/dto/NotificationEvent.java
+++ notification/src/main/java/com/cicarus/notification/dto/NotificationEvent.java
@@ -1,14 +1,55 @@
+// Em: notification/src/main/java/com/cicarus/notification/dto/NotificationEvent.java
+
 package com.cicarus.notification.dto;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class NotificationEvent {
     private Long customerId;
     private String channel;
     private String message;
-    private String recipientEmail; // <- novo campo
+    private String recipientEmail;
+
+    // Construtor sem argumentos
+    public NotificationEvent() {
+    }
+
+    // Construtor com todos os argumentos
+    public NotificationEvent(Long customerId, String channel, String message, String recipientEmail) {
+        this.customerId = customerId;
+        this.channel = channel;
+        this.message = message;
+        this.recipientEmail = recipientEmail;
+    }
+
+    // Getters e Setters
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRecipientEmail() {
+        return recipientEmail;
+    }
+
+    public void setRecipientEmail(String recipientEmail) {
+        this.recipientEmail = recipientEmail;
+    }
 }

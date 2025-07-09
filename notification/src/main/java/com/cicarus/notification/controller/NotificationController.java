7c5956598d1b11f7fc7163ac9c1beff49a698819
itiro-y <itiro_yoshii@hotmail.com>
2025-07-09 14:08:30 -0300

    Adding routes in api-gateway
    Implementing Swagger in Notifications

--- notification/src/main/java/com/cicarus/notification/controller/NotificationController.java
+++ notification/src/main/java/com/cicarus/notification/controller/NotificationController.java
@@ -2,10 +2,13 @@ package com.cicarus.notification.controller;
 
 import com.cicarus.notification.model.Notification;
 import com.cicarus.notification.repository.NotificationRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = " Notification Endpoint")
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController {
@@ -16,11 +19,13 @@ public class NotificationController {
         this.repository = repository;
     }
 
+    @Operation(summary = "Get that returns a customer by its ID")
     @GetMapping
     public List<Notification> getByCustomer(@RequestParam Long customerId) {
         return repository.findByCustomerId(customerId);
     }
 
+    @Operation(summary = "Get that returs a hello notification. For testing purposes")
     @GetMapping("/hello")
     public String helloEndpoint(){
         return "Hello from notification ms!";

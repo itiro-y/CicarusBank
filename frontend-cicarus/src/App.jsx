50b6393d6116795401efbb17538531f5600b63e9
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-07-17 11:01:59 -0300

    auth implementado no login (front)

--- frontend-cicarus/src/App.jsx
+++ frontend-cicarus/src/App.jsx
@@ -13,20 +13,22 @@ import AdminDashboardPage from './pages/AdminDashboardPage.jsx';
 import AdminRoute from './components/AdminRoute.jsx';
 import NearbyAgenciesPage from './pages/NearbyAgenciesPage.jsx';
 
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
     return (
         <AppTheme>
             <Routes>
                 <Route path="/" element={<SignInPage />} />
                 <Route path="/login" element={<SignInPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/user-transactions" element={<UserTransactionsPage />} />
-                <Route path="/admin-transactions" element={<AdminTransactionsPage />} />
-                <Route path="/profile" element={<ProfilePage />} /> {/* Nova rota adicionada */}
-                <Route path="/exchange" element={<ExchangePage />} />
-                <Route path="/loan" element={<LoanSimulationPage />} />
-                <Route path="/loan-tracking" element={<LoanTrackingPage />} />
-                <Route path="/user-card" element={<CardManagementPage />} />
+                <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+                <Route path="/user-transactions" element={<ProtectedRoute><UserTransactionsPage /></ProtectedRoute>} />
+                <Route path="/admin-transactions" element={<ProtectedRoute><AdminTransactionsPage /></ProtectedRoute>} />
+                <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+                <Route path="/exchange" element={<ProtectedRoute><ExchangePage /></ProtectedRoute>} />
+                <Route path="/loan" element={<ProtectedRoute><LoanSimulationPage /></ProtectedRoute>} />
+                <Route path="/loan-tracking" element={<ProtectedRoute><LoanTrackingPage /></ProtectedRoute>} />
+                <Route path="/user-card" element={<ProtectedRoute><CardManagementPage /></ProtectedRoute>} />
                 <Route
                     path="/admin/dashboard"
                     element={

00962073aa81fa2cc593015c4fb0a35eb36ac4ef
guilhermepmoura <guilhermepinheiromoura1@gmail.com>
2025-07-18 14:22:01 -0300

    Ajustes página BenefitsPage.jsx

--- benefits-service/src/main/resources/db/migration/V2__Insert_initial_benefits_data.sql
+++ benefits-service/src/main/resources/db/migration/V2__Insert_initial_benefits_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO benefits (name, description, type, value, valid_until, active) VALUES
-      ('Spotify Premium', 'Resgate Spotify Premium grátis durante 3 meses', 'SERVICE', '60.0', '2027-01-01', '1');
-
-INSERT INTO benefits (name, description, type, value, valid_until, active) VALUES
-          ('Meia entrada cinema', 'Pague meia entrada em qualquer cinema', 'DISCOUNT', '12.50', '2027-01-01', '1');
-
-
-
+       ('Spotify Premium', 'Resgate Spotify Premium grátis durante 3 meses', 'SERVICE', '60.0', '2027-01-01', '1'),
+       ('Amazon Prime por 3 meses grátis', 'Assine Amazon Prime sem custo por 3 meses', 'SERVICE', '60.0', '2027-01-01', '1'),
+       ('Meia entrada cinema', 'Pague meia entrada em qualquer cinema', 'DISCOUNT', '12.50', '2027-01-01', '1'),
+       ('Acesso à salas VIP', 'Acesse salas VIP em aeroportos nacionais com seu cartão Platinum', 'SERVICE', '500.0', '2027-01-01', '1'),
+       ('Milhas em dobro', 'Ganhe o dobro de milhas em compras internacionais', 'CASHBACK', '2.0', '2027-01-01', '1'),
+       ('Seguro viagem gratuito', 'Tenha seguro viagem incluído em viagens internacionais com o cartão', 'INSURANCE', '1000', '2027-01-01', '1'),
+       ('Curso de Educação Financeira', 'Aprenda a organizar suas finanças', 'SERVICE', '25', '2027-01-01', '1');
